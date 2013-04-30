1ccfe117c2581547a190d5329dc25b23511c3e89
AHeinerm@gmail.com <AHeinerm@gmail.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2013-04-30 21:36:25 +0000

    Remove BWTA reference from ExampleAIClient. Add copy step for ExampleAIClient and ExampleAIModule so that the latest sources are used in Release_Binary.

--- branches/bwapi4/bwapi/ExampleAIClient/Source/ExampleAIClient.cpp
+++ branches/bwapi4/bwapi/ExampleAIClient/Source/ExampleAIClient.cpp
@@ -2,7 +2,6 @@
 
 #include <BWAPI.h>
 #include <BWAPI/Client.h>
-//#include <BWTA.h>
 #include <iostream>
 #include <windows.h>
 
@@ -13,18 +12,11 @@ using namespace BWAPI;
 void drawStats();
 void drawBullets();
 void drawVisibilityData();
-//void drawTerrainData();
 void showPlayers();
 void showForces();
 bool show_bullets;
 bool show_visibility_data;
-/*
-bool analyzed;
-bool analysis_just_finished;
-BWTA::Region* home;
-BWTA::Region* enemy_base;
-DWORD WINAPI AnalyzeThread();
-*/
+
 void reconnect()
 {
   while(!BWAPIClient.connect())
@@ -32,6 +24,7 @@ void reconnect()
     Sleep(1000);
   }
 }
+
 int main(int argc, const char* argv[])
 {
   std::cout << "Connecting..." << std::endl;;
@@ -55,12 +48,7 @@ int main(int argc, const char* argv[])
     Broodwar->enableFlag(Flag::UserInput);
     // Uncomment to enable complete map information
     //Broodwar->enableFlag(Flag::CompleteMapInformation);
-    /*
-    //read map information into BWTA so terrain analysis can be done in another thread
-    BWTA::readMap();
-    analyzed=false;
-    analysis_just_finished=false;
-    */
+   
     show_bullets=false;
     show_visibility_data=false;
 
@@ -127,14 +115,8 @@ int main(int argc, const char* argv[])
             } else if (e->getText()=="/show visibility")
             {
               show_visibility_data=!show_visibility_data;
-            } /* else if (e->text=="/analyze")
-            {
-              if (analyzed == false)
-              {
-                Broodwar->printf("Analyzing map... this may take a minute");
-                CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)AnalyzeThread, NULL, 0, NULL);
-              }
-            } */ else
+            } 
+            else
             {
               Broodwar << "You typed \"" << e->getText() << "\"!" << std::endl;
             }
@@ -159,51 +141,51 @@ int main(int argc, const char* argv[])
               Broodwar << "A " << e->getUnit()->getType() << " [" << e->getUnit() << "] has been created at " << e->getUnit()->getPosition() << std::endl;
             else
             {
-              /*if we are in a replay, then we will print out the build order
-              (just of the buildings, not the units).*/
+              // if we are in a replay, then we will print out the build order
+              // (just of the buildings, not the units).
               if (e->getUnit()->getType().isBuilding() && e->getUnit()->getPlayer()->isNeutral()==false)
               {
                 int seconds=Broodwar->getFrameCount()/24;
                 int minutes=seconds/60;
                 seconds%=60;
-                Broodwar->sendText("%.2d:%.2d: %s creates a %s",minutes,seconds,e->getUnit()->getPlayer()->getName().c_str(),e->getUnit()->getType().c_str());
+                Broodwar->sendText("%.2d:%.2d: %s creates a %s", minutes, seconds, e->getUnit()->getPlayer()->getName().c_str(), e->getUnit()->getType().c_str());
               }
             }
             break;
           case EventType::UnitDestroy:
             if (!Broodwar->isReplay())
-              Broodwar->sendText("A %s [%x] has been destroyed at (%d,%d)",e->getUnit()->getType().c_str(),e->getUnit(),e->getUnit()->getPosition().x,e->getUnit()->getPosition().y);
+              Broodwar->sendText("A %s [%x] has been destroyed at (%d,%d)",e->getUnit()->getType().c_str(), e->getUnit(), e->getUnit()->getPosition().x, e->getUnit()->getPosition().y);
             break;
           case EventType::UnitMorph:
             if (!Broodwar->isReplay())
-              Broodwar->sendText("A %s [%x] has been morphed at (%d,%d)",e->getUnit()->getType().c_str(),e->getUnit(),e->getUnit()->getPosition().x,e->getUnit()->getPosition().y);
+              Broodwar->sendText("A %s [%x] has been morphed at (%d,%d)",e->getUnit()->getType().c_str(), e->getUnit(), e->getUnit()->getPosition().x, e->getUnit()->getPosition().y);
             else
             {
-              /*if we are in a replay, then we will print out the build order
-              (just of the buildings, not the units).*/
+              // if we are in a replay, then we will print out the build order
+              // (just of the buildings, not the units).
               if (e->getUnit()->getType().isBuilding() && e->getUnit()->getPlayer()->isNeutral()==false)
               {
                 int seconds=Broodwar->getFrameCount()/24;
                 int minutes=seconds/60;
                 seconds%=60;
-                Broodwar->sendText("%.2d:%.2d: %s morphs a %s",minutes,seconds,e->getUnit()->getPlayer()->getName().c_str(),e->getUnit()->getType().c_str());
+                Broodwar->sendText("%.2d:%.2d: %s morphs a %s" ,minutes, seconds, e->getUnit()->getPlayer()->getName().c_str(), e->getUnit()->getType().c_str());
               }
             }
             break;
           case EventType::UnitShow:
             if (!Broodwar->isReplay())
-              Broodwar->sendText("A %s [%x] has been spotted at (%d,%d)",e->getUnit()->getType().c_str(),e->getUnit(),e->getUnit()->getPosition().x,e->getUnit()->getPosition().y);
+              Broodwar->sendText("A %s [%x] has been spotted at (%d,%d)", e->getUnit()->getType().c_str(), e->getUnit(), e->getUnit()->getPosition().x, e->getUnit()->getPosition().y);
             break;
           case EventType::UnitHide:
             if (!Broodwar->isReplay())
-              Broodwar->sendText("A %s [%x] was last seen at (%d,%d)",e->getUnit()->getType().c_str(),e->getUnit(),e->getUnit()->getPosition().x,e->getUnit()->getPosition().y);
+              Broodwar->sendText("A %s [%x] was last seen at (%d,%d)", e->getUnit()->getType().c_str(), e->getUnit(), e->getUnit()->getPosition().x, e->getUnit()->getPosition().y);
             break;
           case EventType::UnitRenegade:
             if (!Broodwar->isReplay())
-              Broodwar->sendText("A %s [%x] is now owned by %s",e->getUnit()->getType().c_str(),e->getUnit(),e->getUnit()->getPlayer()->getName().c_str());
+              Broodwar->sendText("A %s [%x] is now owned by %s", e->getUnit()->getType().c_str(), e->getUnit(), e->getUnit()->getPlayer()->getName().c_str());
             break;
           case EventType::SaveGame:
-            Broodwar->sendText("The game was saved to \"%s\".",e->getText().c_str());
+            Broodwar->sendText("The game was saved to \"%s\".", e->getText().c_str());
             break;
         }
       }
@@ -216,44 +198,7 @@ int main(int argc, const char* argv[])
 
       drawStats();
       Broodwar->drawTextScreen(300,0,"FPS: %f",Broodwar->getAverageFPS());
-      /*
-      if (analyzed && Broodwar->getFrameCount()%30==0)
-      {
-        //order one of our workers to guard our chokepoint.
-        for(Unitset::iterator i=Broodwar->self()->getUnits().begin();i!=Broodwar->self()->getUnits().end();++i)
-        {
-          if ((*i)->getType().isWorker())
-          {
-            //get the chokepoints linked to our home region
-            std::set<BWTA::Chokepoint*> chokepoints= home->getChokepoints();
-            double min_length=10000;
-            BWTA::Chokepoint* choke=NULL;
 
-            //iterate through all chokepoints and look for the one with the smallest gap (least width)
-            for(std::set<BWTA::Chokepoint*>::iterator c=chokepoints.begin();c!=chokepoints.end();++c)
-            {
-              double length=(*c)->getWidth();
-              if (length<min_length || choke==NULL)
-              {
-                min_length=length;
-                choke=*c;
-              }
-            }
-
-            //order the worker to move to the center of the gap
-            (*i)->rightClick(choke->getCenter());
-            break;
-          }
-        }
-      }
-      if (analysis_just_finished)
-      {
-        Broodwar->printf("Finished analyzing map.");
-        analysis_just_finished=false;
-      }
-      if (analyzed)
-        drawTerrainData();
-      */
       BWAPI::BWAPIClient.update();
       if (!BWAPI::BWAPIClient.isConnected())
       {
@@ -267,30 +212,11 @@ int main(int argc, const char* argv[])
   std::cin.ignore();
   return 0;
 }
-/*
-DWORD WINAPI AnalyzeThread()
-{
-  BWTA::analyze();
 
-  //self start location only available if the map has base locations
-  if (BWTA::getStartLocation(BWAPI::Broodwar->self())!=NULL)
-  {
-    home       = BWTA::getStartLocation(BWAPI::Broodwar->self())->getRegion();
-  }
-  //enemy start location only available if Complete Map Information is enabled.
-  if (BWTA::getStartLocation(BWAPI::Broodwar->enemy())!=NULL)
-  {
-    enemy_base = BWTA::getStartLocation(BWAPI::Broodwar->enemy())->getRegion();
-  }
-  analyzed   = true;
-  analysis_just_finished = true;
-  return 0;
-}
-*/
 void drawStats()
 {
   int line = 0;
-  Broodwar->drawTextScreen(5, 0, "I have %d units:", Broodwar->self()->allUnitCount(UnitTypes::AllUnits) );
+  Broodwar->drawTextScreen(5, 0, "I have %d units:", Broodwar->self()->allUnitCount() );
   for ( UnitType::set::iterator i = UnitTypes::allUnitTypes().begin(); i != UnitTypes::allUnitTypes().end(); ++i )
   {
     int count = Broodwar->self()->allUnitCount(*i);
@@ -327,61 +253,7 @@ void drawVisibilityData()
         Broodwar->drawDotMap(x*32+16, y*32+16, Colors::Red);
     }
 }
-/*
-void drawTerrainData()
-{
-  //we will iterate through all the base locations, and draw their outlines.
-  for(std::set<BWTA::BaseLocation*>::const_iterator i=BWTA::getBaseLocations().begin();i!=BWTA::getBaseLocations().end();++i)
-  {
-    TilePosition p=(*i)->getTilePosition();
-    Position c=(*i)->getPosition();
 
-    //draw outline of center location
-    Broodwar->drawBoxMap(p, p + TilePosition(4,3), Colors::Blue);
-
-    //draw a circle at each mineral patch
-    for(Unitset::iterator j=(*i)->getStaticMinerals().begin();j!=(*i)->getStaticMinerals().end();++j)
-    {
-      Position q=(*j)->getInitialPosition();
-      Broodwar->drawCircleMap(q, 30, Colors::Cyan);
-    }
-
-    //draw the outlines of vespene geysers
-    for(Unitset::iterator j=(*i)->getGeysers().begin();j!=(*i)->getGeysers().end();++j)
-    {
-      TilePosition q=(*j)->getInitialTilePosition();
-      Broodwar->drawBoxMap(q, q + TilePosition(4,2), Colors::Orange);
-    }
-
-    //if this is an island expansion, draw a yellow circle around the base location
-    if ((*i)->isIsland())
-      Broodwar->drawCircleMap(c, 80, Colors::Yellow);
-  }
-
-  //we will iterate through all the regions and draw the polygon outline of it in green.
-  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();++r)
-  {
-    BWTA::Polygon p=(*r)->getPolygon();
-    for(int j=0;j<(int)p.size();++j)
-    {
-      Position point1=p[j];
-      Position point2=p[(j+1) % p.size()];
-      Broodwar->drawLineMap(point1, point2, Colors::Green);
-    }
-  }
-
-  //we will visualize the chokepoints with red lines
-  for(std::set<BWTA::Region*>::const_iterator r=BWTA::getRegions().begin();r!=BWTA::getRegions().end();++r)
-  {
-    for(std::set<BWTA::Chokepoint*>::const_iterator c=(*r)->getChokepoints().begin();c!=(*r)->getChokepoints().end();++c)
-    {
-      Position point1=(*c)->getSides().first;
-      Position point2=(*c)->getSides().second;
-      Broodwar->drawLineMap(point1, point2, Colors::Red);
-    }
-  }
-}
-*/
 void showPlayers()
 {
   Playerset players = Broodwar->getPlayers();
