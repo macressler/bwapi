efef3fd285a53487d56dc7a297d862d57dfaaad0
AHeinerm@gmail.com <AHeinerm@gmail.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2013-06-14 19:44:25 +0000

    Move bwapi4 branch to main trunk.

--- branches/bwapi4/bwapi/BWAPI/Source/Resolution.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "BW/Offsets.h"
-
-void SetResolution(int width, int height);
-
-void DDrawDestroy();
-void DDrawInitialize(int width, int height);
-
--- branches/bwapi4/bwapi/BWAPILIB/Source/Common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define BWAPI_TYPESET(setname,name,...) static const int setname ## struc[] = { __VA_ARGS__ };                    \
-                            static const name::const_set setname(setname ## struc, countof(setname ## struc));
-
-#define BWAPI_TYPEDEF(type,x) const type x(Enum::x)
-
-#ifndef countof
-#define countof(x) ((unsigned int)( sizeof(x)/sizeof((x)[0]) ))
-#endif
